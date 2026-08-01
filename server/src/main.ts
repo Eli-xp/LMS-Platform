bbbdccad1594d0dab3ab7c54136e5ff0ee7da537
Hadixp <hadi.b.2002@gmail.com>
2026-08-02 03:16:29 +0430

    feat: added passport confgis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import config from 'config'
+import config from 'config';
+import * as passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
   await app.listen(config.get<string>('server.PORT'));
   console.log(`app is running on port:${config.get<string>('server.PORT')}(${config.get<string>('server.ENV')}) and database connection is ready`);
 }
