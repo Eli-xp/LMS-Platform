190e7b1dd7faebdcbbda7f5bd31143f76f61a583
Hadixp <hadi.b.2002@gmail.com>
2026-08-02 03:08:24 +0430

    feat: new auth entity
    auth entity created to handle authentication and other things like login and register

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import config from 'config'
 
 @Module({
-  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL')), UsersModule],
+  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL')), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
