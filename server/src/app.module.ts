69e5252c316c3c9de321ecd8b165a53e9cfd9c16
Hadixp <hadi.b.2002@gmail.com>
2026-08-02 00:07:18 +0430

    feat: user entity
    user entity created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import config from 'config'
 
 @Module({
-  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL'))],
+  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL')), UsersModule],
   controllers: [AppController],
   providers: [],
 })
