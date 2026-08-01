81ad312963da824fdc65d2de389b424dcec50b3e
Hadixp <hadi.b.2002@gmail.com>
2026-08-01 20:03:10 +0430

    feat

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import config from 'config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(config.get<string>('server.PORT'));
+  console.log(`app is running on port:${config.get<string>('server.PORT')}(${config.get<string>('server.ENV')}) and database connection is ready ${config.get<string>('server.database.URL')}`);
 }
 bootstrap();
