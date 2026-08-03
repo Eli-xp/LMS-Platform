4a12b3972592c478d3a51fdb776f6889a3336113
Hadixp <hadi.b.2002@gmail.com>
2026-08-04 01:52:43 +0430

    feat: redis files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { RedisModule } from './redis/redis.module';
 import config from 'config'
 
 @Module({
-  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL')), UsersModule, AuthModule],
+  imports: [MongooseModule.forRoot(config.get<string>('server.database.URL')), UsersModule, AuthModule, RedisModule],
   controllers: [AppController],
   providers: [],
 })
