b465139842e1d9714f6380729cc20578c7e69523
Hadixp <hadi.b.2002@gmail.com>
2026-08-03 19:24:16 +0430

    Update: GlobalPipe config added

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from 'config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(config.get<string>('server.PORT'));
   console.log(
     `app is running on port:${config.get<string>('server.PORT')}(${config.get<string>('server.ENV')}) and database connection is ready`,
