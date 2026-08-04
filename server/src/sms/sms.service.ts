7d6d94e94f4f010318d6fc8113d93f98d510168a
Hadixp <hadi.b.2002@gmail.com>
2026-08-04 19:38:17 +0430

    Feat: sms send otp created

--- server/src/sms/sms.service.ts
+++ server/src/sms/sms.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Smsir } from 'sms-typescript';
+import config from 'config';
 
 @Injectable()
-export class SmsService {}
+export class SmsService {
+  private readonly sms: Smsir;
+  constructor() {
+    this.sms = new Smsir(
+      config.get<string>('server.sms.SMS_API_KEY'),
+      config.get<number>('server.sms.SMS_NUMBER'),
+    );
+  }
+  async sendOpt(phone: string, code: string) {
+    return this.sms.sendBulk(`کد ورود شما: ${code}`, [phone]);
+  }
+}
