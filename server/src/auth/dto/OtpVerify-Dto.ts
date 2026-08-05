b83ab71061225ae0dc451764917c836cfeb6a454
Hadixp <hadi.b.2002@gmail.com>
2026-08-05 21:57:18 +0430

    Update: swagger

--- server/src/auth/dto/OtpVerify-Dto.ts
+++ server/src/auth/dto/OtpVerify-Dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class OtpVerify {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({type: String, required: true, maxLength: 6, example: '965884'})
     code!: string
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({type: String, required: true, example: '09363241408'})
     phone!: string
 }
--- server/src/auth/dto/createOpt-Dto.ts
+++ server/src/auth/dto/createOpt-Dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 
 export class CreateOtp{
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({type: String, required: true, example: '09363241408'})
     phone!:string
 }
