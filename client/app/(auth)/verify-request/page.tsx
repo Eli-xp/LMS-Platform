219e9625fa3e3eb54acf6e615a69051edf4f00ee
Alireza <iranexplosm@gmail.com>
2026-08-06 16:50:30 +0330

    feat: add phone number through searchParams

--- client/app/(auth)/verify-request/page.tsx
+++ client/app/(auth)/verify-request/page.tsx
@@ -5,11 +5,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
 import VerifyRequestForm from "./_components/VerifyRequest.form";
 
-const verifyRequestPage = () => {
-  const verificationPhoneNum = 111;
+const verifyRequestPage = async ({
+  searchParams,
+}: {
+  searchParams: { phone?: string };
+}) => {
+  const { phone = "" } = await searchParams;
 
   return (
     <div>
@@ -19,12 +22,11 @@ const verifyRequestPage = () => {
             Vertify your login{" "}
           </CardTitle>
           <CardDescription className="px-10 pt-2">
-            Enter verification code we sent to your <br /> phone number:{" "}
-            {verificationPhoneNum}
+            Enter verification code we sent to your <br /> phone number: {phone}
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <VerifyRequestForm />
+          <VerifyRequestForm phone={phone} />
         </CardContent>
       </Card>
     </div>
