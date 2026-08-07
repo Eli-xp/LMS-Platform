0e9cc639f9e815b7469e3da1668b0a4aa1cb17fe
Hadixp <hadi.b.2002@gmail.com>
2026-08-07 14:48:15 +0430

    Update: new cookie strategy

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -14,12 +14,26 @@ import { RegisterDto } from './dto/registerDto';
 import { OtpVerify } from './dto/OtpVerify-Dto';
 import { CreateOtp } from './dto/createOpt-Dto';
 import type { Request, Response } from 'express';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/verifyOtp')
+  @ApiResponse({
+    type: Object,
+    status: 201,
+    example: {
+      user: {
+        name: 'userName',
+        _id: 'userId',
+        phone: 'user Phone number',
+        refreshToken: 'user hashed refreshToken',
+      },
+    },
+  })
+  @ApiOperation({ summary: 'verify OTP Code' })
   async otpVerify(
     @Body() otpVerify: OtpVerify,
     // passthrough let you send set cookie using res while returning another data
@@ -28,18 +42,29 @@ export class AuthController {
     const { token, refreshToken, user } =
       await this.authService.otpVerify(otpVerify);
     // send as cookies
-    res.cookie('refresh_token', refreshToken, { httpOnly: false,});
-    res.cookie('access_token', token, { httpOnly: false });
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    });
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    });
     // return user
-    return { user };
+    return user;
   }
 
   @Post('/sendOtp')
+  @ApiResponse({ type: String, status: 201, example: 'OTP send successfully' })
+  @ApiOperation({ summary: 'Send OTP Code to Users phone number' })
   sendOtp(@Body() createOtp: CreateOtp) {
     return this.authService.sendOtp(createOtp);
   }
 
   @Post('/refresh')
+  @ApiOperation({ summary: 'regenerate access token' })
   refresh(@Req() req: Request) {
     return this.authService.refresh(req.cookies.refresh_token);
   }
