6f7bf584a51c6b805a66f5a93800356dd6aa899c
Alireza <iranexplosm@gmail.com>
2026-08-13 03:47:49 +0330

    fix update schema propert based on zod update

--- client/schemas/course.schema.ts
+++ client/schemas/course.schema.ts
@@ -19,34 +19,32 @@ export const courseCategories = [
 export const courseSchema = z.object({
   title: z
     .string()
-    .min(3, { message: "Title must be at least 3 characters long" })
-    .max(100, { message: "Title must be at most 100 characters long" }),
+    .min(3, { error: "Title must be at least 3 characters long" })
+    .max(100, { error: "Title must be at most 100 characters long" }),
   slug: z
     .string()
-    .min(3, { message: "Slug must be at least 3 characters long" })
-    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, { message: "Invalid Slug format" }),
-  category: z.enum(courseCategories, { message: "Category is required" }),
+    .min(3, { error: "Slug must be at least 3 characters long" })
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, { error: "Invalid Slug format" }),
+  category: z.enum(courseCategories, { error: "Category is required" }),
   description: z
     .string()
-    .min(3, { message: "Description must be at least 3 characters long" }),
+    .min(3, { error: "Description must be at least 3 characters long" }),
   smallDescription: z
     .string()
-    .min(3, { message: "Small Description must be at least 3 characters long" })
+    .min(3, { error: "Small Description must be at least 3 characters long" })
     .max(200, {
-      message: "Small Description must be at most 200 characters long",
+      error: "Small Description must be at most 200 characters long",
     }),
-  fileKey: z.string().min(1, { message: "File is required" }),
-  price: z.coerce
-    .number()
-    .min(1, { message: "Price must be a positive number" }),
+  fileKey: z.string().min(1, { error: "File is required" }),
+  price: z.coerce.number().min(1, { error: "Price must be a positive number" }),
   duration: z.coerce
     .number()
-    .min(1, { message: "Duration must be at least 1 hour" })
-    .max(500, { message: "Duration must be at most 500 hours" }),
+    .min(1, { error: "Duration must be at least 1 hour" })
+    .max(500, { error: "Duration must be at most 500 hours" }),
   level: z.enum(courseLevels, {
-    message: "Level is required",
+    error: "Level is required",
   }),
   status: z.enum(courseStatus, {
-    message: "Status is required",
+    error: "Status is required",
   }),
 });
