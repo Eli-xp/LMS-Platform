ab0b186c11a7df39d3e369e610ea3edfa0da4b44
Hadixp <hadi.b.2002@gmail.com>
2026-08-13 17:08:33 +0430

    Update media.service.ts

--- server/src/media/media.service.ts
+++ server/src/media/media.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import config from 'config';
 import { randomUUID } from 'crypto';
 @Injectable()
 export class MediaService {
     private s3 = new S3Client({
-        region: 'auto',
+        region: 'default',
         endpoint: config.get<string>('server.aws.END_POINT'),
         credentials: {
             accessKeyId: config.get<string>('server.aws.ACCESS_KEY'),
@@ -16,15 +16,12 @@ export class MediaService {
 
 
     async createUploadUrl(originalName: string, contentType: string) {
-        const extension = originalName.split('.').pop();
-        const fileKey = `Assets/${randomUUID()}.${extension}`
-        const command = new PutObjectCommand({
+        const command = new GetObjectCommand({
             Bucket: config.get<string>('server.aws.BUCKET'),
-            Key: fileKey,
-            ContentType: contentType,
+            Key: originalName,
         })
 
         const uploadUrl = await getSignedUrl(this.s3, command, { expiresIn: 3600 })
-        return {uploadUrl,fileKey}
+        return {uploadUrl}
     }
 }
