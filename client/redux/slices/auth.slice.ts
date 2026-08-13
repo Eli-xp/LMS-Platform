76cf733eed02d648158651ac26f7317fbe131b01
Alireza <iranexplosm@gmail.com>
2026-08-13 17:58:11 +0330

    fix: update auth slice reducers with loading state

--- client/redux/slices/auth.slice.ts
+++ client/redux/slices/auth.slice.ts
@@ -10,19 +10,23 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: {
     user: null,
+    loading: true,
   },
   reducers: {
     // after initial enter
     initializeUser: (state, action: PayloadAction) => {
       state.user = action.payload;
+      state.loading = false;
     },
     // after changes
     setUser: (state, action: PayloadAction) => {
       state.user = action.payload;
+      state.loading = false;
     },
     // after logout
     clearUser: (state) => {
       state.user = null;
+      state.loading = false;
     },
   },
 });
