aab18d92ca5b29d6423475f255de8f5b43d90504
Alireza <iranexplosm@gmail.com>
2026-08-15 01:37:48 +0330

    feat: modularize admin course card section

--- client/app/admin/courses/page.tsx
+++ client/app/admin/courses/page.tsx
@@ -1,7 +1,18 @@
 import { buttonVariants } from "@/components/ui/button";
+// import { adminGetCourses } from "@/services/admin/adminGetCourses.api";
 import Link from "next/link";
+import AdminCourseCard from "./_components/AdminCourseCard";
+import { adminGetCourses } from "@/schemas/course.schema";
+import { sampleDataCourses } from "./sampleData";
+import z from "zod";
 
-const CoursesPage = () => {
+// sample data
+const data: z.infer<typeof adminGetCourses>[] = sampleDataCourses;
+
+const CoursesPage = async () => {
+  // const data = await adminGetCourses();
+  console.log(data);
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,6 +24,11 @@ const CoursesPage = () => {
       </div>
       <div>
         <span>Here you will see all of the courses</span>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+        {data.map((course) => (
+          <AdminCourseCard key={course.id} {...course} />
+        ))}
       </div>
     </>
   );
