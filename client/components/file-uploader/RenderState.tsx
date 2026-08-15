51c7f950689fc974e2979e864c55f8b4d01171ad
Alireza <iranexplosm@gmail.com>
2026-08-16 02:18:51 +0330

    feat: update render states for file upload pre-view

--- client/components/file-uploader/RenderState.tsx
+++ client/components/file-uploader/RenderState.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
-import { CloudUploadIcon, ImageIcon } from "lucide-react";
+import { CloudUploadIcon, ImageIcon, XIcon } from "lucide-react";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export const RenderState = ({ isDragActive }: { isDragActive: boolean }) => {
   return (
@@ -35,6 +37,57 @@ export const RenderErrorState = () => {
       <p className="text-xl mt-3 text-muted-foreground">
         Click or darg file to retry
       </p>
+    </div>
+  );
+};
+
+export const RenderUploadedState = ({
+  previewUrl,
+  setFileState,
+  onFileChange,
+}: {
+  previewUrl: string | undefined;
+}) => {
+  const onClickFunc = (e) => {
+    e.stopPropagation();
+
+    console.log("onClickfunc test");
+    setFileState({
+      file: null,
+      fileType: "image",
+      id: null,
+      objectUrl: undefined,
+      progress: 0,
+      error: false,
+      uploading: false,
+      isDeleting: false,
+    });
+
+    onFileChange();
+  };
+
+  return (
+    <div className="relative flex items-center justify-center h-full w-full">
+      <Button
+        onClick={onClickFunc}
+        type="button"
+        variant={"destructive"}
+        size={"icon"}
+        className={cn(
+          "absolute top-4 right-4 bg-red-500 hover:bg-red-500/50 z-50 cursor-pointer",
+        )}
+      >
+        <XIcon className="size-4" />
+      </Button>
+      <Image
+        src={previewUrl || "/logo.svg"}
+        alt="Uploaded File"
+        width={250}
+        height={250}
+        className="object-contain p-2"
+        draggable={false}
+        unoptimized
+      />
     </div>
   );
 };
