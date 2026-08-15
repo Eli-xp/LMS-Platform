e9e0ab1478d7be02cad6ebdd70c9ff684d7a12a0
Hadixp <hadi.b.2002@gmail.com>
2026-08-15 20:53:10 +0430

    Update course.controller.ts

--- server/src/course/course.controller.ts
+++ server/src/course/course.controller.ts
@@ -22,13 +22,12 @@ import { Types } from 'mongoose';
 import { ConfirmUploadDto } from 'src/media/DTO/confirmUploadDto';
 import { UsersService } from 'src/users/users.service';
 
-
 @Controller('course')
 export class CourseController {
   constructor(
     private readonly courseService: CourseService,
     private readonly mediaService: MediaService,
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
   ) {}
 
   @Post('/upload-url')
@@ -42,8 +41,11 @@ export class CourseController {
     },
   })
   async createUploadUrl(@Body() createUploadUrlDto: CreateUploadUrlDto) {
-    const {uploadUrl, fileKey} = await this.mediaService.createUploadUrl(createUploadUrlDto.originalName,createUploadUrlDto.contentType);
-    return {uploadUrl, fileKey}
+    const { uploadUrl, fileKey } = await this.mediaService.createUploadUrl(
+      createUploadUrlDto.originalName,
+      createUploadUrlDto.contentType,
+    );
+    return { uploadUrl, fileKey };
   }
 
   @UseGuards(AuthGuard('jwt'))
@@ -63,45 +65,50 @@ export class CourseController {
         smallDescription: 'about js...',
         slug: 'string',
         status: 'Draft',
-        thumbNail:{
-          originalName:'js events',
-          contentType:'video/mp4'
-        }
+        thumbNail: {
+          originalName: 'js events',
+          contentType: 'video/mp4',
+        },
       },
     },
   })
   async create(@Body() createCourseDto: CreateCourseDto, @Req() req: Request) {
-    const { uploadUrl,fileKey } = await this.mediaService.createUploadUrl(createCourseDto.thumbNail!.originalName,createCourseDto.thumbNail!!.contentType)
+    const { uploadUrl, fileKey } = await this.mediaService.createUploadUrl(
+      createCourseDto.thumbNail!.originalName,
+      createCourseDto.thumbNail!!.contentType,
+    );
     const { newCourse } = await this.courseService.create(createCourseDto);
     const { userId } = req.user as JwtUser;
     newCourse.userId = new Types.ObjectId(userId);
     newCourse.thumbnail = fileKey;
     await newCourse.save();
     const user = await this.usersService.findById(userId);
-    if(!user){
-      throw new NotFoundException('user not found')
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
-    user.courses?.push(newCourse._id)
-    await user.save()
-    return { message: 'new course created', uploadUrl ,newCourse };
+    user.courses?.push(newCourse._id);
+    await user.save();
+    return { message: 'new course created', uploadUrl, newCourse };
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Post('/upload-complete')
-  async uploadComplete(@Body() 
-  confirmUploadDto: ConfirmUploadDto,@Req() req:Request) {
+  async uploadComplete(
+    @Body()
+    confirmUploadDto: ConfirmUploadDto,
+    @Req() req: Request,
+  ) {
     const { userId } = req.user as JwtUser;
-    const {course} = await this.mediaService.completeUpload(confirmUploadDto,userId);
-    return {message: 'upload completed', course};
+    const { course } = await this.mediaService.completeUpload(
+      confirmUploadDto,
+      userId,
+    );
+    return { message: 'upload completed', course };
   }
-
 
   @UseGuards(AuthGuard('jwt'))
   @Get('/view-url')
-  async getViewUrl(@Query('fileKey') fileKey: string){
-    return this.mediaService.createViewUrl(fileKey)
+  async getViewUrl(@Query('fileKey') fileKey: string) {
+    return this.mediaService.createViewUrl(fileKey);
   }
-
-
-
 }
