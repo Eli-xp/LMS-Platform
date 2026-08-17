ca9035d091f1c76bcf324143780fd45f71978174
Alireza <iranexplosm@gmail.com>
2026-08-17 04:17:19 +0330

    fix: update authorization on server side

--- client/services/admin/course/adminGetCourses.api.ts
+++ client/services/admin/course/adminGetCourses.api.ts
@@ -1,5 +1,29 @@
-// Only admin API
-export const adminGetCourses = () => {};
+import { cookies } from "next/headers";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// expected response::
-// [{id, title, smalDescription,duration,level, status, price, fileKey, slug}, {...},...]
+export const adminGetCourses = async () => {
+  // Only admin API
+
+  console.log("adminGetCourses ran");
+
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.toString();
+
+  const res = await fetch(`${API_URL}/admin/courses`, {
+    headers: {
+      Cookie: accessToken,
+    },
+    method: "GET",
+  });
+
+  // console.log(res);
+
+  if (!res.ok) {
+    throw new Error(`Faild to Get Courses: ${res.status}`);
+  }
+
+  const data = res.json();
+  console.log(data);
+
+  return data;
+};
