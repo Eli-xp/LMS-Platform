ddf054bfe4a88691ca29b138ea9fc03f5a6b7172
Alireza <iranexplosm@gmail.com>
2026-08-19 20:51:18 +0330

    refactor: split schemas for each route

--- client/schemas/course.schema.ts
+++ client/schemas/course.schema.ts
@@ -18,16 +18,6 @@ export const courseCategories = [
 // note: MongoDB wil create=> id, createdAt, updatedAt
 
 //  /admin/courses/create File Upload
-// export const thumbNail = z.union([
-//   z.string().min(1, { error: "Invalid thumbNail URL" }),
-
-//   z.object({
-//     file: z.instanceof(File),
-//     originalName: z.string().min(1, { error: "Invalid File Name" }),
-//     contentType: z.string().min(1, { error: "Invalid File Type" }),
-//     size: z.number().min(1, { error: "Invalid File Size" }),
-//   }),
-// ]);
 
 export const courseCreateSchema = z.object({
   title: z
@@ -72,13 +62,20 @@ export const courseCreateSchema = z.object({
     .optional(),
 });
 
+//
+//
+//
 
+export const editThumbnail = z.union([
+  z.string().min(1, { error: "Invalid thumbNail URL" }),
 
-
-
-
-
-
+  z.object({
+    file: z.instanceof(File),
+    originalName: z.string().min(1, { error: "Invalid File Name" }),
+    contentType: z.string().min(1, { error: "Invalid File Type" }),
+    size: z.number().min(1, { error: "Invalid File Size" }),
+  }),
+]);
 export const AdminGetCourseSchema = z.object({
   title: z
     .string()
@@ -98,7 +95,7 @@ export const AdminGetCourseSchema = z.object({
     .max(200, {
       error: "Small Description must be at most 200 characters long",
     }),
-  thumbnail: z.string().min(1,{error:"Invalid thumbnail URL"}),
+  thumbnail: editThumbnail,
   price: z.coerce.number().min(1, { error: "Price must be a positive number" }),
   duration: z.coerce
     .number()
@@ -117,27 +114,43 @@ export const AdminGetCourseSchema = z.object({
     .optional(),
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
 // /admin/courses
-export const adminGetCourses = courseCreateSchema.pick({
-  _id: true,
-  title: true,
-  smallDescription: true,
-  duration: true,
-  level: true,
-  status: true,
-  price: true,
-  thumbNail: true,
-  slug: true,
+export const adminGetCourses = z.object({
+  title: z
+    .string()
+    .min(3, { error: "Title must be at least 3 characters long" })
+    .max(100, { error: "Title must be at most 100 characters long" }),
+  slug: z
+    .string()
+    .min(3, { error: "Slug must be at least 3 characters long" })
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, { error: "Invalid Slug format" }),
+  category: z.enum(courseCategories, { error: "Category is required" }),
+  description: z
+    .string()
+    .min(3, { error: "Description must be at least 3 characters long" }),
+  smallDescription: z
+    .string()
+    .min(3, { error: "Small Description must be at least 3 characters long" })
+    .max(200, {
+      error: "Small Description must be at most 200 characters long",
+    }),
+  thumbnail: z.object({
+    viewUrl: z.string().min(1, { error: "Invalid viewUrl" }),
+  }),
+  price: z.coerce.number().min(1, { error: "Price must be a positive number" }),
+  duration: z.coerce
+    .number()
+    .min(1, { error: "Duration must be at least 1 hour" })
+    .max(500, { error: "Duration must be at most 500 hours" }),
+  level: z.enum(courseLevels, {
+    error: "Level is required",
+  }),
+  status: z.enum(courseStatus, {
+    error: "Status is required",
+  }),
+
+  _id: z
+    .string()
+    .min(3, { error: "id must be at least 3 characters long" })
+    .optional(),
 });
