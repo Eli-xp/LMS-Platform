a966995b96b8881989361d48039219beeb6d2851
Hadixp <hadi.b.2002@gmail.com>
2026-08-21 02:48:58 +0430

    Feat: lessonKey created

---
+++ server/src/lesson/schema/lessonSchema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+
+@Schema({ timestamps: true })
+export class Lesson extends Document{
+    @Prop({ type: String, required: true })
+    title!: string;
+    @Prop({ type: String, required: true })
+    description!: string;
+    @Prop({ type: String })
+    thumbnailKey?: string;
+    @Prop({ type: String })
+    videoKey?: string;
+    @Prop({ type: Number, required: true })
+    position!: number;
+    @Prop({ type: Types.ObjectId, ref: 'Chapter', required: true })
+    chapterId!: Types.ObjectId;
+}
+
+export const LessonSchema = SchemaFactory.createForClass(Lesson);
