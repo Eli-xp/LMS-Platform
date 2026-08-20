4279abb8b7acc20dbdb9004647a902ebdc10fda3
Alireza <iranexplosm@gmail.com>
2026-08-20 20:32:30 +0330

    test: create sample data for course structure edit

---
+++ client/app/admin/courses/[courseId]/edit/_components/sapmleData.ts
@@ -0,0 +1,70 @@
+export const sampleDataCourseStructure = {
+  id: "course_001",
+  title: "Modern JavaScript From Zero to Advanced",
+
+  chapters: [
+    {
+      id: "chapter_001",
+      title: "JavaScript Fundamentals",
+      position: 1,
+      lessons: [
+        {
+          id: "lesson_001",
+          title: "Introduction to JavaScript",
+          position: 1,
+        },
+        {
+          id: "lesson_002",
+          title: "Variables and Data Types",
+          position: 2,
+        },
+        {
+          id: "lesson_003",
+          title: "Operators",
+          position: 3,
+        },
+      ],
+    },
+
+    {
+      id: "chapter_002",
+      title: "Functions",
+      position: 2,
+      lessons: [
+        {
+          id: "lesson_004",
+          title: "Function Basics",
+          position: 1,
+        },
+        {
+          id: "lesson_005",
+          title: "Arrow Functions",
+          position: 2,
+        },
+      ],
+    },
+
+    {
+      id: "chapter_003",
+      title: "Asynchronous JavaScript",
+      position: 3,
+      lessons: [
+        {
+          id: "lesson_006",
+          title: "Callbacks",
+          position: 1,
+        },
+        {
+          id: "lesson_007",
+          title: "Promises",
+          position: 2,
+        },
+        {
+          id: "lesson_008",
+          title: "Async and Await",
+          position: 3,
+        },
+      ],
+    },
+  ],
+};
