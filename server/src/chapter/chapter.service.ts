3a2541ef2976d4a33f7330eff5d998dcf0ad145a
Hadixp <hadi.b.2002@gmail.com>
2026-08-21 03:37:47 +0430

    Update: lesson and chapter default services removed

--- server/src/chapter/chapter.service.ts
+++ server/src/chapter/chapter.service.ts
@@ -4,23 +4,5 @@ import { UpdateChapterDto } from './dto/update-chapter.dto';
 
 @Injectable()
 export class ChapterService {
-  create(createChapterDto: CreateChapterDto) {
-    return 'This action adds a new chapter';
-  }
-
-  findAll() {
-    return `This action returns all chapter`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} chapter`;
-  }
-
-  update(id: number, updateChapterDto: UpdateChapterDto) {
-    return `This action updates a #${id} chapter`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} chapter`;
-  }
+  
 }
--- server/src/lesson/lesson.service.ts
+++ server/src/lesson/lesson.service.ts
@@ -4,23 +4,5 @@ import { UpdateLessonDto } from './dto/update-lesson.dto';
 
 @Injectable()
 export class LessonService {
-  create(createLessonDto: CreateLessonDto) {
-    return 'This action adds a new lesson';
-  }
-
-  findAll() {
-    return `This action returns all lesson`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} lesson`;
-  }
-
-  update(id: number, updateLessonDto: UpdateLessonDto) {
-    return `This action updates a #${id} lesson`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} lesson`;
-  }
+  
 }
