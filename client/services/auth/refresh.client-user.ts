4369b83b4fae360e2d8c227a6e028556ad4a9d07
Alireza <iranexplosm@gmail.com>
2026-08-21 14:59:51 +0330

    feat: create refresh API file for client component

---
+++ client/services/auth/refresh.client-user.ts
@@ -0,0 +1,10 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const refreshAccessToken = async () => {
+  const res = await fetch(`${API_URL}/auth/refresh`, {
+    method: "POST",
+    credentials: "include",
+  });
+
+  return res.ok;
+};
