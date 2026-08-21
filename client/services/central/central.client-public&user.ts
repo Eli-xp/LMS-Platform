63fe238928dcaeee0a489b6df68b204aaffd9f7f
Alireza <iranexplosm@gmail.com>
2026-08-21 14:51:58 +0330

    feat: create central API layer for client requests

---
+++ client/services/central/central.client-public&user.ts
@@ -0,0 +1,73 @@
+import { refreshAccessToken } from "../auth/refresh.client-user";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+let refreshPromise: Promise<boolean> | null = null;
+
+const refresh = async () => {
+  if (!refreshPromise) {
+    refreshPromise = refreshAccessToken().finally(() => {
+      refreshPromise = null;
+    });
+  }
+  return refreshPromise;
+};
+
+const handleAuthFailure = async () => {
+  try {
+    await fetch(`${API_URL}/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+interface centralClientAPIOptions extends RequestInit {
+  auth?: boolean;
+}
+
+export const centralClientAPI = async (
+  endpoint: string,
+  options: centralClientAPIOptions = {},
+) => {
+  console.log("centralClientAPI Called");
+
+  const { auth = true, ...fetchOptions } = options;
+
+  let response = await fetch(`${API_URL}${endpoint}`, {
+    ...fetchOptions,
+    credentials: "include",
+  });
+
+  // clg test
+  console.log(response);
+
+  if (!auth) {
+    return response;
+  }
+
+  //  if authorized
+  if (response.status !== 401) {
+    return response;
+  }
+
+  //// if unauthorized (401) - Access Token Expired
+
+  // Refresh Token Request
+  const refreshed = await refresh();
+
+  // if Refresh Token Expired
+  if (!refreshed) {
+    await handleAuthFailure();
+    return;
+  }
+
+  // if Refresh Token Valid - Retry original request
+  response = await fetch(`${API_URL}${endpoint}`, {
+    ...fetchOptions,
+    credentials: "include",
+  });
+
+  return response;
+};
