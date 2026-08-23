2d0ef878ba3c5456b241853aa68641e4bd42b61f
Hadixp <hadi.b.2002@gmail.com>
2026-08-24 03:21:58 +0430

    Update structureDto.ts

--- server/src/chapter/dto/structureDto.ts
+++ server/src/chapter/dto/structureDto.ts
@@ -1,8 +1,14 @@
+import {  ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsOptional } from "class-validator";
 
 export class StructureDto{
     @IsArray()
     @IsOptional()
+    @ApiProperty({type: [Object], example:[
+        {id:'123asd',position:2, title:'test'},
+        {id:'123asd',position:2, title:'test'},
+        {id:'123asd',position:2, title:'test'}
+    ]})
     chapters?:{
         id: string
         position: number
@@ -10,6 +16,11 @@ export class StructureDto{
     }[];
     @IsArray()
     @IsOptional()
+    @ApiProperty({type: [Object], example:[
+        {id:'123asd',position:2, title:'test'},
+        {id:'123asd',position:2, title:'test'},
+        {id:'123asd',position:2, title:'test'}
+    ]})
     lessons?:{
         id: string
         position: number
