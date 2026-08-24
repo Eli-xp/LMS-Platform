ad4200d548bdde66492957b4f1356a8e65cc6aee
Alireza <iranexplosm@gmail.com>
2026-08-24 10:08:27 +0330

    refactor: relocate courses route for pagination

---
+++ client/app/admin/courses/page/[coursesPage]/_components/CoursesList.tsx
@@ -0,0 +1,36 @@
+import { adminGetCourses } from "@/services/admin/course/GetCourses.server-admin";
+import AdminCourseCard from "./AdminCourseCard";
+import z from "zod";
+import CoursesPagination from "./CoursesPagination";
+
+export const CoursesList = async ({ currentPageNum }: { currentPageNum: number }) => {
+  console.log(currentPageNum);
+  console.log(typeof currentPageNum);
+  let coursesData;
+
+  try {
+    const { data }: z.infer<typeof adminGetCourses> =
+      await adminGetCourses(currentPageNum);
+    coursesData = data;
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+        {coursesData?.courses?.length > 0 &&
+          coursesData.courses.map((course) => (
+            <AdminCourseCard key={course._id} {...course} />
+          ))}
+      </div>
+
+      {/*  courses pagination */}
+      <CoursesPagination
+        currentPage={currentPageNum}
+        totalPage={coursesData?.pageCount}
+      />
+    </>
+  );
+};
