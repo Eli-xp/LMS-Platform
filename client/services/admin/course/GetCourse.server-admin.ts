01b32cffd3baa4deb8f33913ad531e54cf407ff1
Alireza <iranexplosm@gmail.com>
2026-08-24 12:01:33 +0330

    feat: add API call to get single course structure

--- client/services/admin/course/GetCourse.server-admin.ts
+++ client/services/admin/course/GetCourse.server-admin.ts
@@ -1,6 +1,6 @@
 import { centralServerAPI } from "@/services/central/central.server-public&user";
 
-export const adminGetCourse = async (id: string) => {
+export const adminGetCourse_basic = async (id: string) => {
   // Only admin API
 
   console.log("adminGetCourse ran");
@@ -19,6 +19,25 @@ export const adminGetCourse = async (id: string) => {
 
   if (!res.response.ok) {
     throw new Error(`Faild to Get Courses: ${res.response.status}`);
+  }
+
+  return { type: res.type, data: await res.response.json() };
+};
+
+export const adminGetCourse_structure = async (courseId: string) => {
+  // only admin
+
+  console.log(courseId);
+  console.log(typeof courseId);
+  const id = courseId;
+
+  // Request Validation
+  const res = await centralServerAPI(`/admin/course/structure/${id}`, {
+    method: "GET",
+  });
+  console.log(res);
+  if (!res.response.ok) {
+    throw new Error(`Failed to edit course as admin:${res.status}`);
   }
 
   return { type: res.type, data: await res.response.json() };
