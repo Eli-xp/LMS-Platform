f9d9cc11e0b2ad5888ca2e725cc037940f6a3e76
Alireza <iranexplosm@gmail.com>
2026-08-26 02:37:35 +0330

    feat: update server dal with new refresh logic

--- client/services/central/central.server-public&user.ts
+++ client/services/central/central.server-public&user.ts
@@ -9,16 +9,35 @@ export const centralServerAPI = async (
 ) => {
   console.log("centralServerAPI called");
 
+  // get cookies
   const cookieStore = await cookies();
 
-  const response = await fetch(`${API_URL}${endpoint}`, {
+  // request
+  let response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
     headers: { ...options.headers, Cookie: cookieStore.toString() },
   });
 
+  // clg test
+  console.log(response);
+
   if (response.status === 401) {
-    return { type: "auth-required" };
+    ////////
+
+    console.log("centralClientAPI - Before Rtry");
+    // if Refresh Token Valid - Retry original request
+    response = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers: { ...options.headers, Cookie: cookieStore.toString() },
+    });
+
+    console.log("centralClientAPI - After Rtry");
+    console.log(response);
   }
 
-  return { type: "success", response };
+  if (!response.ok) {
+    console.error(response.status);
+  }
+
+  return response;
 };
