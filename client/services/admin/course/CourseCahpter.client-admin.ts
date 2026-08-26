7b57db9c0d5d1ee559b7eaa2ffbb1628bb328ca4
Alireza <iranexplosm@gmail.com>
2026-08-27 02:46:28 +0330

    feat: create chapter API file for course edit

---
+++ client/services/admin/course/CourseCahpter.client-admin.ts
@@ -0,0 +1,24 @@
+import { centralClientAPI } from "@/services/central/central.client-public&user";
+import { createChapterSchema } from "@/schemas/course.schema";
+import z from "zod";
+
+export const createChapter = async (
+  values: z.infer<typeof createChapterSchema>,
+) => {
+  // only admin
+
+  console.log(values);
+
+  // Request Validation
+  const res = await centralClientAPI(`/chapter/create`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(values),
+  });
+
+  if (!res?.ok) {
+    throw new Error(`Failed to edit course as admin:${res?.status}`);
+  }
+
+  return res.status;
+};
