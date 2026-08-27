dfb3ce455a22ca11e9e6396b1e9c97b7e56ff098
Hadixp <hadi.b.2002@gmail.com>
2026-08-27 21:24:10 +0430

    Update create-lesson.dto.ts

--- server/src/lesson/dto/create-lesson.dto.ts
+++ server/src/lesson/dto/create-lesson.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsObject, IsNotEmpty, IsString } from "class-validator";
+import { IsObject, IsNotEmpty, IsString, IsOptional } from "class-validator";
 
 export class CreateLessonDto {
     @IsString()
@@ -7,9 +7,11 @@ export class CreateLessonDto {
     @ApiProperty({ type: String, required: true, example: "Lesson 1" })
     title!: string;
     @IsString()
+    @IsOptional()
     @ApiProperty({ type: String, example: "this lesson is about OOP" })
     description?: string;
     @IsObject()
+    @IsOptional()
     @ApiProperty({ type: Object, example: { originalname: "flower.png", contentType: "image/png" } })
     thumbnailObject?: {
         originalname: string;
