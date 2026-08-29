1336116983f2515687a254a2138a79d99a39a032
Hadixp <hadi.b.2002@gmail.com>
2026-08-29 23:58:22 +0430

    Update users.controller.ts

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -27,31 +27,4 @@ export class UsersController {
   createProfile(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get('courses')
-  @ApiOperation({summary: 'return courses with Published status only'})
-  async fineAll(@Query('page') page: number, @Query('limit') limit: number){
-    const courses = await this.CourseModel.find({status: 'Published'}).select('title slug category smallDescription thumbnail price duration level').skip((page - 1) * limit).limit(limit).lean();
-    const courseCount = await this.CourseModel.countDocuments();
-    const pageCount = Math.ceil(courseCount / limit);
-    const coursesWithThumbnail = await Promise.all(
-      courses.map(async (course) => {
-        const thumbnailUrl = await this.mediaService.createViewUrl(
-          course.thumbnail,
-        );
-
-        return {
-          ...course,
-          thumbnail: thumbnailUrl
-        };
-      }),
-    );
-    return {
-      courses: coursesWithThumbnail,
-      pageCount,
-    };
-    
-
-  }
 }
