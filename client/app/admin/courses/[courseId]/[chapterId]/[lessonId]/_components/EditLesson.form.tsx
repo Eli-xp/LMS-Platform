7beaaa31a35a176b95375a2f494c8da7aea2e35c
Alireza <iranexplosm@gmail.com>
2026-08-29 03:46:39 +0330

    feat: improve API calls based on input changes

--- client/app/admin/courses/[courseId]/[chapterId]/[lessonId]/_components/EditLesson.form.tsx
+++ client/app/admin/courses/[courseId]/[chapterId]/[lessonId]/_components/EditLesson.form.tsx
@@ -2,7 +2,7 @@
 
 import FileUploader from "@/components/file-uploader/FileUploader";
 import RichTextEditor from "@/components/rich-text-editor/Editor";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -25,22 +25,18 @@ import {
 import { adminCourseFileVerification } from "@/services/admin/course/CourseFileVerification.client-admin";
 import { adminPutLesson } from "@/services/admin/course/lesson/PutLesson.client-admin";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowLeft, X } from "lucide-react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { X } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 
-const EditLessonForm = ({
-  data,
-  courseId,
-  chapterId,
-}: {
-  data: getLessonSchemaType;
-  courseId: string;
-  chapterId: string;
-}) => {
+const EditLessonForm = ({ data }: { data: getLessonSchemaType }) => {
+  const { courseId, chapterId } = useParams<{
+    courseId: string;
+    chapterId: string;
+  }>();
+
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const router = useRouter();
 
@@ -100,7 +96,7 @@ const EditLessonForm = ({
         "videoObject" in changedValues
       ) {
         // 1_1) edit course request
-        console.log("CourseEditForm API:: thumbnail adn video changed");
+        console.log("CourseEditForm API:: thumbnail and video changed");
         try {
           const { thumbnailObject, videoObject, ...rest } = changedValues;
           const changedValuesforserver = {
@@ -115,27 +111,26 @@ const EditLessonForm = ({
             },
             chapterId,
           };
-
           console.log(changedValuesforserver);
-          // backend request
+
+          toast.info("Lesson Edit Requested");
           const editLessonRes = await adminPutLesson({
             changedValuesforserver,
             id: data._id,
           });
           console.log(editLessonRes);
-          toast.info("Lesson Edit Requested");
 
           console.log("adminPostCourseVerification called");
 
           // send thumbnail
           const thumbnailPromisse = await adminCourseFileVerification({
-            link: editLessonRes,
+            link: editLessonRes.result,
             file: values.thumbnailObject.file,
           });
 
           // send video
           const videoPromisse = await adminCourseFileVerification({
-            link: editLessonRes.videoKey,
+            link: editLessonRes.result.videoKey,
             file: values.videoObject.file,
           });
 
@@ -154,9 +149,9 @@ const EditLessonForm = ({
           ) {
             toast.success("Mission Successfull.");
             // Reset for inputs
-            // form.reset();
+            form.reset();
             // Redirect to admin courses
-            // router.replace("/admin/courses");
+            router.replace("/admin/courses");
             setIsSubmitting(false);
           } else {
             console.error(thumbnailVerification);
@@ -169,76 +164,126 @@ const EditLessonForm = ({
         }
       }
 
-      //   // 2) if thumbnail changed
-      //   if ("thumbnail" in changedValues) {
-      //     try {
-      //       // 2_1) edit course request
-      //       console.log("CourseEditForm API:: thumbnail changed");
+      // 2) if only thumbnail changed
+      else if ("thumbnailObject" in changedValues) {
+        try {
+          // 2_1) edit course request
+          console.log("CourseEditForm API:: only thumbnail changed");
 
-      //       const { thumbnail, ...rest } = changedValues;
-      //       const changedValuesforserver = {
-      //         ...rest,
-      //         thumbNail: {
-      //           originalName: thumbnail?.originalName,
-      //           contentType: thumbnail?.contentType,
-      //         },
-      //         size: thumbnail?.size,
-      //       };
+          const { thumbnailObject, ...rest } = changedValues;
+          const changedValuesforserver = {
+            ...rest,
+            thumbnailObject: {
+              originalname: thumbnailObject?.originalName,
+              contentType: thumbnailObject?.contentType,
+            },
+            chapterId,
+          };
 
-      //       const editCourseRes = await adminEditCourse_basic(
-      //         // changed inputs
-      //         changedValuesforserver,
-      //         // lessonId
-      //         id:data._id,
-      //       );
-      //       console.log(editCourseRes);
-      //       toast.success("Course Edited Successfully");
+          toast.info("Lesson Edit Requested");
+          const editLessonRes = await adminPutLesson({
+            changedValuesforserver,
+            id: data._id,
+          });
+          console.log(editLessonRes);
 
-      //       //
-      //       console.log("adminPostCourseVerification called");
-      //       const createCourseResverification = await adminCourseFileVerification(
-      //         {
-      //           link: editCourseRes,
-      //           file: values.thumbnail.file,
-      //         },
-      //       );
-      //       console.log(createCourseResverification);
+          console.log("adminPostCourseVerification called");
+          const createCourseResverification = await adminCourseFileVerification(
+            {
+              link: editLessonRes.result,
+              file: values.thumbnailObject.file,
+            },
+          );
+          console.log(createCourseResverification);
 
-      //       // if storage 204
-      //       if (createCourseResverification.status === 204) {
-      //         // Reset for inputs
-      //         form.reset();
-      //         // Redirect to admin courses
-      //         router.replace("/admin/courses");
-      //         setIsSubmitting(false);
-      //       } else {
-      //         console.error(createCourseResverification.status);
-      //         setIsSubmitting(false);
-      //       }
-      //     } catch (error) {
-      //       console.error(error);
-      //       setIsSubmitting(false);
-      //     }
-      //   }
+          // if storage 204
+          if (createCourseResverification.status === 204) {
+            toast.success("Mission Successfull.");
+            // Reset for inputs
+            form.reset();
+            // Redirect to admin courses
+            router.replace("/admin/courses");
+            setIsSubmitting(false);
+          } else {
+            setIsSubmitting(false);
+          }
+        } catch (error) {
+          console.error(error);
+          setIsSubmitting(false);
+        }
+      }
 
-      // 2) if thumbnail did not change
-      //   else {
-      //     try {
-      //       console.log("CourseEditForm API:: thumbnail did not change");
-      //       setIsSubmitting(true);
-      //       const editCourseRes = await adminEditCourse_basic(
-      //         changedValues,
-      //         course_id,
-      //       );
-      //       console.log(editCourseRes);
-      //       toast.success("Course Edited Successfully");
-      //       setIsSubmitting(false);
-      //       router.replace("/admin/courses/");
-      //     } catch (error) {
-      //       console.error(error);
-      //       setIsSubmitting(false);
-      //     }
-      //   }
+      // 2) if only video changed
+      else if ("videoObject" in changedValues) {
+        try {
+          // 2_1) edit course request
+          console.log("CourseEditForm API:: only video changed");
+
+          const { videoObject, ...rest } = changedValues;
+          const changedValuesforserver = {
+            ...rest,
+            videoObject: {
+              originalname: videoObject?.originalName,
+              contentType: videoObject?.contentType,
+            },
+            chapterId,
+          };
+          console.log(changedValuesforserver);
+
+          toast.info("Lesson Edit Requested");
+          const editLessonRes = await adminPutLesson({
+            changedValuesforserver,
+            id: data._id,
+          });
+          console.log(editLessonRes);
+
+          console.log("adminPostCourseVerification called");
+          const createCourseResverification = await adminCourseFileVerification(
+            {
+              link: editLessonRes.result.videoKey,
+              file: values.videoObject.file,
+            },
+          );
+          console.log(createCourseResverification);
+
+          // if storage 204
+          if (createCourseResverification.status === 204) {
+            toast.success("Mission Successfull.");
+            // Reset for inputs
+            form.reset();
+            // Redirect to admin courses
+            router.replace("/admin/courses");
+            setIsSubmitting(false);
+          } else {
+            setIsSubmitting(false);
+          }
+        } catch (error) {
+          console.error(error);
+          setIsSubmitting(false);
+        }
+      }
+
+      // 3) if media(thumbnail/video) did not change
+      else {
+        try {
+          console.log("CourseEditForm API:: non-media changes");
+          setIsSubmitting(true);
+
+          const changedValuesforserver = { ...changedValues, chapterId };
+          console.log(changedValuesforserver);
+          const editCourseRes = await adminPutLesson({
+            changedValuesforserver,
+            id: data._id,
+          });
+          console.log(editCourseRes);
+          toast.success("Course Edited Successfully");
+          setIsSubmitting(false);
+          router.replace("/admin/courses/");
+        } catch (error) {
+          console.error(error);
+          setIsSubmitting(false);
+        }
+      }
 
       // if course had no change
     } else {
@@ -252,145 +297,135 @@ const EditLessonForm = ({
   };
 
   return (
-    <div>
-      <Link
-        className={buttonVariants({ variant: "outline", className: "mb-6 " })}
-        href={`/admin/courses/${courseId}/edit`}
-      >
-        <ArrowLeft className="size-4" />
-        <span>Go Back</span>
-      </Link>
+    <Card>
+      <CardHeader>
+        <CardTitle>Lesson Configuration</CardTitle>
+        <CardDescription>
+          Configure the video adn description for this lesson.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="CourseEditFormID"
+          onSubmit={form.handleSubmit(onSubmit, (error) => {
+            console.log("VALIDATION ERRORS:", error, form.getValues());
+          })}
+          className="space-y-6"
+        >
+          <FieldGroup>
+            <Controller
+              name="title"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="title">
+                    Lesson Name<span className="text-destructive">*</span>
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="title"
+                    aria-invalid={fieldState.invalid}
+                    inputMode="text"
+                    type="text"
+                    placeholder="Complete Next.js 16 Course"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Lesson Configuration</CardTitle>
-          <CardDescription>
-            Configure the video adn description for this lesson.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form
-            id="CourseEditFormID"
-            onSubmit={form.handleSubmit(onSubmit, (error) => {
-              console.log("VALIDATION ERRORS:", error, form.getValues());
-            })}
-            className="space-y-6"
-          >
-            <FieldGroup>
-              <Controller
-                name="title"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="title">
-                      Lesson Name<span className="text-destructive">*</span>
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="title"
-                      aria-invalid={fieldState.invalid}
-                      inputMode="text"
-                      type="text"
-                      placeholder="Complete Next.js 16 Course"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+            <Controller
+              name="description"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <RichTextEditor field={field} filedLabel={"Description"} />
 
-              <Controller
-                name="description"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <RichTextEditor field={field} filedLabel={"Description"} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-
-              <Controller
-                name="thumbnailObject"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="thumbnailObject">
-                      Thumbnail Image
-                      <span className="text-destructive">
-                        *{" "}
-                        <span className="text-muted-foreground text-xs">
-                          (only 1 image max: 5mb - .png .jpg .jpeg )
-                        </span>
+            <Controller
+              name="thumbnailObject"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="thumbnailObject">
+                    Thumbnail Image
+                    <span className="text-destructive">
+                      *{" "}
+                      <span className="text-muted-foreground text-xs">
+                        (only 1 image max: 5mb - .png .jpg .jpeg )
                       </span>
-                    </FieldLabel>
-                    <FileUploader
-                      initialURL={data.thumbnailKey}
-                      onFileChange={field.onChange}
-                      filTypeAccepted="image"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+                    </span>
+                  </FieldLabel>
+                  <FileUploader
+                    initialURL={data.thumbnailKey}
+                    onFileChange={field.onChange}
+                    filTypeAccepted="image"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-              <Controller
-                name="videoObject"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="videoObject">
-                      Video
-                      <span className="text-destructive">
-                        *{" "}
-                        <span className="text-muted-foreground text-xs">
-                          (only 1 image max: 5mb - .png .jpg .jpeg )
-                        </span>
+            <Controller
+              name="videoObject"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="videoObject">
+                    Video
+                    <span className="text-destructive">
+                      *{" "}
+                      <span className="text-muted-foreground text-xs">
+                        (only 1 image max: 5mb - .png .jpg .jpeg )
                       </span>
-                    </FieldLabel>
-                    <FileUploader
-                      initialURL={data.videoKey}
-                      onFileChange={field.onChange}
-                      filTypeAccepted="video"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+                    </span>
+                  </FieldLabel>
+                  <FileUploader
+                    initialURL={data.videoKey}
+                    onFileChange={field.onChange}
+                    filTypeAccepted="video"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-              {/* form submit button */}
-              <div className="w-full grid grid-cols-2">
-                <Button
-                  type="submit"
-                  form="CourseEditFormID"
-                  disabled={isSubmitting}
-                  className="cursor-pointer w-full"
-                >
-                  Save Lesson
-                </Button>
-                <Button
-                  variant={"destructive"}
-                  type="button"
-                  disabled={isSubmitting}
-                  onClick={CourseCancelEdit}
-                  className="cursor-pointer w-full"
-                >
-                  Cancel <X size={16} />
-                </Button>
-              </div>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+            {/* form submit button */}
+            <div className="w-full grid grid-cols-2">
+              <Button
+                type="submit"
+                form="CourseEditFormID"
+                disabled={isSubmitting}
+                className="cursor-pointer w-full"
+              >
+                {isSubmitting ? "Saving.." : "Save Lesson"}
+              </Button>
+              <Button
+                variant={"destructive"}
+                type="button"
+                disabled={isSubmitting}
+                onClick={CourseCancelEdit}
+                className="cursor-pointer w-full"
+              >
+                Cancel <X size={16} />
+              </Button>
+            </div>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 
