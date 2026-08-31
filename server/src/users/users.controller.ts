2c0da9dbc09a27c509bf6e9abdbe3d5373f702e7
Hadixp <hadi.b.2002@gmail.com>
2026-08-31 22:03:44 +0430

    Update users.controller.ts

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -27,28 +27,33 @@ export class UsersController {
     private readonly usersService: UsersService,
     private readonly mediaService: MediaService,
     @InjectModel(Course.name) private readonly CourseModel: Model<Course>,
-    @InjectModel(User.name) private readonly UserModel: Model<User>
+    @InjectModel(User.name) private readonly UserModel: Model<User>,
   ) {}
 
   @Post('createProfile')
   async completeProfile(@Body() updateUserDto: UpdateUserDto) {
-    const user = await this.usersService.completeProfile(updateUserDto)
+    const user = await this.usersService.completeProfile(updateUserDto);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('suggestion')
-  @ApiOperation({summary: 'return courses without pain ones'})
-  async getUserSuggest(@Req() req: Request){
-    const {userId} = req.user as JwtUser;
-    const user = await this.UserModel.findById(userId).select('paidCourses').lean();
-    if(!user){
-      throw new NotFoundException('user not found')
+  @ApiOperation({ summary: 'return courses without pain ones' })
+  async getUserSuggest(@Req() req: Request) {
+    const { userId } = req.user as JwtUser;
+    const user = await this.UserModel.findById(userId).lean();
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
-    const paidCoursesId = user.paidCourses;
+    const paidCoursesId = user?.paidCourses ?? [];
     const courses = await this.CourseModel.find({
       status: 'Published',
-      _id: {$nin: paidCoursesId}
-    });
+      _id: { $nin: paidCoursesId },
+    })
+      .limit(3)
+      .sort({ createdAt: -1 })
+      .select(
+        'title slug category smallDescription thumbnail price duration level',
+      );
     return courses;
   }
 }
