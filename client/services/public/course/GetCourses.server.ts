07886717241eed38d4e9e340fb7b943e118575fd
Alireza <iranexplosm@gmail.com>
2026-08-31 16:43:43 +0330

    refactor: update public get courses API with dynamic limit number

--- client/services/public/course/GetCourses.server.ts
+++ client/services/public/course/GetCourses.server.ts
@@ -2,11 +2,11 @@ import { centralServerAPI } from "@/services/central/central.server-public&user"
 
 //// Get Only Published Courses for public
 // (ordered based on newest result)
-export const PublicGetCourses_API = async (currentPageNum: number) => {
+export const PublicGetCourses_API = async ({currentPageNum, limit}: {currentPageNum: number, limit:number}) => {
   console.log("PublicGetCourses_API ran");
   console.log(currentPageNum);
   const page = currentPageNum;
-  const res = await centralServerAPI(`/public/courses/?page=${page}&limit=10`, {
+  const res = await centralServerAPI(`/public/courses/?page=${page}&limit=${limit}`, {
     method: "GET",
   });
 
